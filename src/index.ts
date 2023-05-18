aba26a3dfe7cf9021bd37a81f7c414ee1fb73c3f
Jesse Longacre <jesse@jlte.ch>
2023-05-18 16:21:17 -0700

    Updates.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,15 @@
-import {useDrupal, DrupalProvider, DrupalLoadDefaults} from "./hooks/useDrupal";
-import {useDrupalJsonApi} from "./hooks/useDrupalJsonApi";
-import {useDrupalCartToken} from "./hooks/useDrupalCartToken";
-import {useDrupalCsrfToken} from "./hooks/useDrupalCsrfToken";
-import {useDrupalCartLineItem} from "./hooks/useDrupalCartLineItem";
-import {useDrupalCheckout} from "./hooks/useDrupalCheckout";
-import {useDrupalCheckoutFlow} from "./hooks/useDrupalCheckoutFlow";
-import {useDrupalCustomerProfiles} from "./hooks/useDrupalCustomerProfiles";
-import {useDrupalProduct} from "./hooks/useDrupalProduct";
-import {useDrupalRestApi} from "./hooks/useDrupalRestApi";
-import {useDrupalSearchApi} from "./hooks/useDrupalSearchApi";
-import {useDrupalUser} from "./hooks/useDrupalUser";
-import {useDrupalCarts} from "./hooks/useDrupalCarts";
-
-export {
-  useDrupal,
-  useDrupalUser,
-  useDrupalCarts,
-  useDrupalJsonApi,
-  useDrupalSearchApi,
-  useDrupalProduct,
-  useDrupalCheckout,
-  useDrupalCartLineItem,
-  useDrupalCsrfToken,
-  useDrupalCartToken,
-  useDrupalCustomerProfiles,
-  useDrupalCheckoutFlow,
-  useDrupalRestApi,
-  DrupalProvider,
-  DrupalLoadDefaults
-}
+export {default as DrupalEntity} from './service/DrupalEntity';
+export {useDrupal, DrupalProvider, DrupalLoadDefaults} from "./hooks/useDrupal";
+export {useDrupalJsonApi} from "./hooks/useDrupalJsonApi";
+export {useDrupalCartToken} from "./hooks/useDrupalCartToken";
+export {useDrupalCsrfToken} from "./hooks/useDrupalCsrfToken";
+export {useDrupalCartLineItem} from "./hooks/useDrupalCartLineItem";
+export {useDrupalCheckout} from "./hooks/useDrupalCheckout";
+export {useDrupalCheckoutFlow} from "./hooks/useDrupalCheckoutFlow";
+export {useDrupalCustomerProfiles} from "./hooks/useDrupalCustomerProfiles";
+export {useDrupalProduct} from "./hooks/useDrupalProduct";
+export {useDrupalRestApi} from "./hooks/useDrupalRestApi";
+export {useDrupalSearchApi} from "./hooks/useDrupalSearchApi";
+export {useDrupalUser} from "./hooks/useDrupalUser";
+export {useDrupalCarts} from "./hooks/useDrupalCarts";
+export * from "./helpers";

ea891d81e7dc95fcedb8e901e9f2e8d5c2f71874
Jesse Longacre <jesse@jlte.ch>
2023-05-18 09:26:54 -0700

    Introduce webpack.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+};
